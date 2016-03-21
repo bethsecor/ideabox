a2366cb7ece26ed195f68613ede0797712f1bfb7
Beth Secor <elizabeth.a.secor@gmail.com>
2016-03-21 14:07:16 -0600

    Show endpoint for ideas api complete

--- spec/controllers/api/v1/ideas_controller_spec.rb
+++ spec/controllers/api/v1/ideas_controller_spec.rb
@@ -10,9 +10,28 @@ RSpec.describe Api::V1::IdeasController, type: :controller do
 
     ideas = JSON.parse(response.body)
     first_idea = ideas.first
+    second_idea = ideas.last
 
     expect(first_idea['title']).to eq(idea_1.title)
     expect(first_idea['body']).to eq(idea_1.body)
     expect(first_idea['quality']).to eq(idea_1.quality)
+
+    expect(second_idea['title']).to eq(idea_2.title)
+    expect(second_idea['body']).to eq(idea_2.body)
+    expect(second_idea['quality']).to eq(idea_2.quality)
+  end
+
+  it "will show show of an idea" do
+    idea_1 = create(:idea)
+
+    get :show, id: idea_1.id, format: :json
+
+    expect(response.status).to eq(200)
+
+    idea = JSON.parse(response.body)
+
+    expect(idea['title']).to eq(idea_1.title)
+    expect(idea['body']).to eq(idea_1.body)
+    expect(idea['quality']).to eq(idea_1.quality)
   end
 end
